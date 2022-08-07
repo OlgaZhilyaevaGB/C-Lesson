c5df4fe765036650accf36c93e47d19b86f063b5
Olga <Celena2004@mail.ru>
2022-08-07 14:59:00 +0300

    Добавлена восьмая задача

---
+++ zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number=int.Parse(Console.ReadLine());
+int count=2;
+while (count<=number)
+  {
+         
+        Console.WriteLine($" Число {count} четное");
+        count=count+2;   
+  }
+
+
